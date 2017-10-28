b416a03159d6c0bddbc9073e945726c2244ca8c2
Mistra <manoyasamistra@gmail.com>
2017-10-28 11:29:28 +0700

    Hello React

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
-const http = require('http');
+//const http = require('http');
+//const fs = require('fs');
+
 
 //event emiters
 //streams
 //cluster
 
-const server = http.createServer();
+//const server = http.createServer();
+//server.on('request', (req, res) => {
+//    res.write('Hello Node\n');
+ //   setTimeout(() => {
+   //     res.write('I am still on .....');
+     //   res.end();
+   // }, 3000)
+//});
+//server.listen(8080);
+
+/* const server = http.createServer();
 server.on('request', (req, res) => {
-    res.write('Hello Node\n');
-    setTimeout(() => {
-        res.write('I am still on ..... /n');
-        res.end();
-    }, 3000)
+    res.end(fs.readFileSync(__dirname + '/../client/index.html'));
 });
-server.listen(8080);
+server.listen(8080) */
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('client'));
+app.listen(8080);
